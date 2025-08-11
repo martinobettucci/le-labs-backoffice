bcc24fcc8c200c03d3e0ecf0000dfe7f2e0d6708
LaoTseu0 <antho-permaculture@hotmail.com>
2025-08-11 16:03:33 +0200

    Add allowed hosts in Vite configuration for server

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: '0.0.0.0',
+    allowedHosts: ['admin.labs-hub'],
     watch: {
       usePolling: true,
       interval: 1000,
